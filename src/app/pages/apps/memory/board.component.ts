e42e956ec573b8fb229c892e2345685c56770bf5
JoseRFJuniorLLMs <web2ajax@gmail.com>
2024-07-02 04:50:45 +0000

    BUG GAME

--- src/app/pages/apps/memory/board.component.ts
+++ src/app/pages/apps/memory/board.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { CardService } from '../card.service';
+import { CardService } from './card.service';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDividerModule } from '@angular/material/divider';
-import { CardComponent } from '../card/card.component';
+import { CardComponent } from './card.component';
 
 @Component({
   selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss'],
+  templateUrl: 'board.component.html',
+  styleUrls: ['board.component.scss'],
   standalone: true,
   imports: [CommonModule, MatButtonModule, MatRadioModule, MatDividerModule, CardComponent]
 })
@@ -28,6 +28,7 @@ export class BoardComponent implements OnInit {
 
   newGame() {
     this.cards = this.cardService.getCards(this.cardCount);
+    console.log('BoardComponent - New game started, cards:', this.cards);
     this.moves = 0;
     this.remainingPairs = this.cardCount;
     this.calculateWidth();
@@ -35,10 +36,12 @@ export class BoardComponent implements OnInit {
 
   calculateWidth() {
     this.boardWidth = (this.cards.length / 2) * 110; // Adjust as needed
+    console.log('BoardComponent - Calculated board width:', this.boardWidth);
   }
 
   cardFlipped(card: any) {
     this.moves++;
+    console.log('BoardComponent - Card flipped, card:', card);
     // Logic to check for pairs and update remainingPairs
   }
 }
--- src/app/pages/apps/memory/card.component.ts
+++ src/app/pages/apps/memory/card.component.ts
@@ -1,24 +1,31 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
+  templateUrl: 'card.component.html',
+  styleUrls: ['card.component.scss'],
   standalone: true,
   imports: [CommonModule, MatCardModule]
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
   @Input() card: any;
   @Input() isFlipped: boolean = false;
   @Output() flipped = new EventEmitter<void>();
 
   ngOnInit() {
-    console.log('Card:', this.card);
+    console.log('CardComponent - Initialized with card:', this.card);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['card']) {
+      console.log('CardComponent - Card input changed:', changes['card'].currentValue);
+    }
   }
 
   onClick() {
+    console.log('CardComponent - Card clicked:', this.card);
     this.isFlipped = !this.isFlipped;
     this.flipped.emit();
   }
