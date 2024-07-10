87506b4e9b6cb7fb0e110544b14eb47e99f8d2a4
JoseRFJuniorLLMs <web2ajax@gmail.com>
2024-07-10 12:47:27 +0000

    books3 refactory BUG GRAMATICA UPDATE LOGIN

--- src/app/pages/apps/student/form/edit-student-dialog.component.ts
+++ src/app/pages/apps/student/form/edit-student-dialog.component.ts
@@ -1,11 +1,20 @@
-import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-edit-student-dialog',
@@ -19,7 +28,10 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule, MatPaginatorModule, MatSortModule, 
+    MatButtonToggleModule, MatCardModule, MatListModule, 
+    MatDialogModule
   ]
 })
 export class EditStudentDialogComponent {
